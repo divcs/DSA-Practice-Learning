285601fceeddecefd649d162f7a5d7f3790dc00e
Divya <singhdivya10aug2002@gmail.com>
2024-08-05 10:00:07 +0530

    Striver's que updated

---
+++ Strivers/Step 1/Basic maths/count_digits.js
@@ -0,0 +1,15 @@
+function count_digits(num){
+
+ let count=0
+
+ while(num>0){
+  count=count+1
+
+  num=Math.floor(num/10)
+ }
+return count
+}
+
+const number= 3356773
+const digits = count_digits(number)
+console.log(digits)
