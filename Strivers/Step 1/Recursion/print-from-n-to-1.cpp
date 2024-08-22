a06fa85846dc45e5674602216894b60864f066d6
Divya <singhdivya10aug2002@gmail.com>
2024-08-22 20:52:19 +0530

    Recursion

--- Strivers/Step 1/Recursion/print-from-n-to-1.cpp
+++ Strivers/Step 1/Recursion/print-from-n-to-1.cpp
@@ -3,21 +3,20 @@ using namespace std;
 
 void printNum(int i, int n)
 {
- if (i > n)
+ if (i < 0)
   return;
  cout << i << endl;
- printNum(i + 1, n);
+ printNum(i - 1, n);
 }
 
 int main()
 {
  int n, i;
- cout << "Enter the number upto which numbers are to be printed:";
+ cout << "Enter the number from where numbers are to be printed:";
  cin >> n;
 
- printNum(i = 1, n);
+ printNum(i = n, n);
  // Your code goes here
 
  return 0;
 }
-
