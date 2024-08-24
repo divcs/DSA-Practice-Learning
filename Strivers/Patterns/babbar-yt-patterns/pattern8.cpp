40a613cd9a9c09b22bd3b052f84cecc7f69e1145
Divya <singhdivya10aug2002@gmail.com>
2024-08-24 19:35:10 +0530

    Pattern 8: IInd method

--- Strivers/Patterns/babbar-yt-patterns/pattern8.cpp
+++ Strivers/Patterns/babbar-yt-patterns/pattern8.cpp
@@ -1,3 +1,5 @@
+// Method 1: with third variable
+
 #include <iostream>
 using namespace std;
 
@@ -13,13 +15,41 @@ int main()
   int value = row; // since every first col value of each row is the same as row value
   while (col <= row)
   {
-   cout << value;
+   cout << row << " ";
    value = value + 1;
    col = col + 1;
   }
 
   cout << endl;
   row = row + 1;
+ }
+ return 0;
+}
+
+// Method 2: without third variable
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int n;
+ cin >> n;
+
+ int row = 1;
+ while (row <= n)
+ {
+  // The starting value for each row is the same as the row number
+  int value = row;
+
+  // Print out the numbers for the current row
+  while (value < row + row) // Loop from `value` to `row + row - 1` we are increasing the column values until it is less than twice of row number
+  {
+   cout << value << " ";
+   value = value + 1; // Increment the value directly
+  }
+
+  cout << endl;  // Print a new line after each row
+  row = row + 1; // Move to the next row
  }
  return 0;
 }
